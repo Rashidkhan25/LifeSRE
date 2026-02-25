af8342519b310bf2fde4cbb609dda0663cba6f50
rashidkhan25 <khanrashidejaz@gmail.com>
2026-02-26 00:23:17 +0530

    Deployment

--- backend/testWhatsApp.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const sendWhatsApp = require("./services/whatsappService");
-
-sendWhatsApp("Test message from LifeSRE");
-console.log(process.env.USER_WHATSAPP_NUMBER);
